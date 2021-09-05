5996218183ddf8b87caa0a07116275cbd85e9e7b
orbitan <81489935+orbitan@users.noreply.github.com>
2021-09-05 21:47:26 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         passwordGenerator test = new passwordGenerator();
-        test.requiredTypes();
+        test.fertigGeneriertesPasswort(8, false, false, true, false);
 
 
     }
