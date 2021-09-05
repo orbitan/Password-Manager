dd953f5602b86ac6c5f1a88477bbd87cc597f336
orbitan <81489935+orbitan@users.noreply.github.com>
2021-09-05 21:46:07 +0200

    Update passwordGenerator.java

--- passwordGenerator.java
+++ passwordGenerator.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class passwordGenerator {
@@ -8,78 +7,51 @@ public class passwordGenerator {
     public boolean zahlen;
     public boolean letters;
     public boolean specialCases;
+    public boolean upperCases;
+
+// TODO Nummerliste ist eigentlich überflüssig. Besser sowas wie random.choice(0, 10); Das Gleiche gilt für ABC- und abc-Listen;
+// TODO Exceptions falls requiredLenght < 1;
 
 
-
-
-    public void  requiredTypes(){  // checkt, aus welchen Typen und wievielen Zeichen das Passwort bestehen soll;
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Aus wievielen Zeichen soll das Passwort bestehen? ");  //Zeichenzahl
-        int requiredLength = scanner.nextInt();
-        this.requiredLength = requiredLength;
-
-        System.out.println("Soll das Passwort Buchstaben enthalten? (Enter 'true' or 'false') "); //Buchstaben ja/nein
-        boolean letters = scanner.nextBoolean();
-        this.letters = letters;
-
-        System.out.println("Soll das Passwort besondere Zeichen enthalten? (Enter 'true' or 'false') "); //Zeichen ja/nein
-        boolean specialCases = scanner.nextBoolean();
-        this.specialCases = specialCases;
-
-        System.out.println("Soll das Passwort Zahlen enthalten? (Enter 'true' or 'false') ");
-        boolean zahlen = scanner.nextBoolean();
-        this.zahlen = specialCases;
-
-        fertigGeneriertesPasswort(requiredLength, letters, specialCases, zahlen);
-    }
-
-    public void fertigGeneriertesPasswort(int requiredLength, boolean letters, boolean specialCases, boolean zahlen){ //generiert ein Passwort auf Basis der definierten Attribute
+    public StringBuilder fertigGeneriertesPasswort(int requiredLength, boolean letters, boolean specialCases, boolean zahlen, boolean upperCases) { //generiert ein Passwort auf Basis der definierten Attribute
         List<Character> letterList = new ArrayList<Character>(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'));
-        List<Character> specialCharactertest = new ArrayList<Character>(Arrays.asList('!', '#', '+', '*', '?', '&', '$', '-', '_'));
+        List<Character> upperCaseLetters = new ArrayList<Character>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'));
+        List<Character> specialCharacters = new ArrayList<Character>(Arrays.asList('!', '#', '+', '*', '?', '&', '$', '-', '_'));
         List<Character> numbers = new ArrayList<Character>(Arrays.asList('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
+        List<Character> passwortListe = new ArrayList<Character>();
         Random random = new Random();
-        StringBuilder passwort = new StringBuilder();
+
         int i = 0;
-
-        if(letters && specialCases && zahlen){
-            letterList.addAll(specialCharactertest);
-            letterList.addAll(numbers);
-            while(i < requiredLength){
-                int randomElement = random.nextInt(letterList.size());
-                passwort.append(letterList.get(randomElement));
-                i ++;
+        int randomElement;
+        while (i < requiredLength) {
+            if (letters) {
+                randomElement = random.nextInt(letterList.size());
+                passwortListe.add(letterList.get(randomElement));
+                i++;
             }
-            System.out.println(passwort);
-
-        }else if(letters && zahlen){
-            letterList.addAll(numbers);
-            while(i < requiredLength){
-                int randomElement = random.nextInt(letterList.size());
-                passwort.append(letterList.get(randomElement));
-                i ++;
+            if (zahlen && i < requiredLength) {
+                //randomElement = random.nextInt(numbers.size());
+                randomElement = random.nextInt(10);
+                passwortListe.add(numbers.get(randomElement));
+                i++;
             }
-            System.out.println(passwort);
-
-        }else if(zahlen){
-            while(i < requiredLength){
-                int randomElement = random.nextInt(numbers.size());
-                passwort.append(numbers.get(randomElement));
-                i ++;
+            if (specialCases && i < requiredLength) {
+                randomElement = random.nextInt(specialCharacters.size());
+                passwortListe.add(specialCharacters.get(randomElement));
+                i++;
             }
-            System.out.println(passwort);
-
-        }else if (specialCases){
-            while(i < requiredLength){
-                int randomElement = random.nextInt(specialCharactertest.size());
-                passwort.append(specialCharactertest.get(randomElement));
-                i ++;
+            if (upperCases && i < requiredLength) {
+                randomElement = random.nextInt(letterList.size());
+                passwortListe.add(upperCaseLetters.get(randomElement));
+                i++;
             }
-            System.out.println(passwort);
-
-
         }
-
+        StringBuilder password = new StringBuilder();
+        while (password.length() < requiredLength) {
+            randomElement = random.nextInt(passwortListe.size());
+            password.append(passwortListe.get(randomElement));
+            passwortListe.remove(randomElement);
+        }
+        return password;
     }
 }
-
