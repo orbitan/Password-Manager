3f52cea0f4b3d196386ae85a32242429f8679435
Caro <81607208+code-caro@users.noreply.github.com>
2021-09-26 11:50:06 +0200

    Update PasswordManager.java

--- src/passwordManager/PasswordManager.java
+++ src/passwordManager/PasswordManager.java
@@ -7,6 +7,23 @@ public class PasswordManager{
     public static void main(String[] args){
         PasswordManager pwMan = new PasswordManager();
         pwMan.mode = Mode.get(args);
-        pwMan.mode.run();  
+        pwMan.mode.run(); 
+    
+        EncryptionPassword ec = new EncryptionPassword();
+        //Insert password and prepare it for encryption and decryption
+        ec.createPassword();   
+
+        SecretKeySpec key = EncryptionPassword.createSecretKey(ec.getPassword(), ec.getSalt(), ec.getIterationCount(), ec.getKeyLength());
+
+        //Test
+        String originalPassword = String.valueOf(ec1.getPassword());
+        System.out.println("Original password: " + originalPassword);
+
+        String encryptedPassword = EncryptionPassword.encrypt(originalPassword, key);
+        System.out.println("Encrypted password: " + encryptedPassword);
+
+        String decryptedPassword = EncryptionPassword.decrypt(encryptedPassword, key);
+        System.out.println("Decrypted password: " + decryptedPassword);
     }
+ 
 }
